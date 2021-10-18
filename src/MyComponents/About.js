0a79047bbb5ca238b19cf13b2e3d859eed6e7cb0
AxayPatoliya <76492978+AxayPatoliya@users.noreply.github.com>
2021-10-18 12:51:55 +0530

    Initial Deploy

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -6,6 +6,8 @@ export default function About() {
         <div className='container'>
             Build by <span><h1 className="text-success">Axay Patoliya</h1></span>
             Technology used - <b className="text-info">React.js</b>
+            <hr />
+            <p>Find source code <a href="https://github.com/AxayPatoliya/React-todo-app">here</a></p>
         </div>
     )
 }
